5bb63d023b31c67553a6faf0ba318202e2103a43
Suric zhang <372081349@qq.com>
2016-09-25 19:39:46 +0800

    Initial Commit

---
+++ FLyingSnail/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FLyingSnail
+//
+//  Created by Suric on 16/9/25.
+//  Copyright © 2016年 snail. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

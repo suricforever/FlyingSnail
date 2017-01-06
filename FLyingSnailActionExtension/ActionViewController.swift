b76722c05d8fd0568b052e1b362090bd4ae63585
Suric zhang <372081349@qq.com>
2017-01-06 13:51:13 +0800

    add Action target

---
+++ FLyingSnailActionExtension/ActionViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ActionViewController.swift
+//  FLyingSnailActionExtension
+//
+//  Created by Suric on 2017/1/6.
+//  Copyright © 2017年 snail. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+import Alamofire
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sendURLRequest()
+    
+        // Get the item[s] we're handling from the extension context.
+        
+        // For example, look for an image and place it into an image view.
+        // Replace this with something appropriate for the type[s] your extension supports.
+        var imageFound = false
+        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
+            for provider in item.attachments! as! [NSItemProvider] {
+                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
+                    // This is an image. We'll load it, then place it in our image view.
+                    weak var weakImageView = self.imageView
+                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
+                        OperationQueue.main.addOperation {
+                            if let strongImageView = weakImageView {
+                                if let imageURL = imageURL as? URL {
+                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
+                                }
+                            }
+                        }
+                    })
+                    
+                    imageFound = true
+                    break
+                }
+            }
+            
+            if (imageFound) {
+                // We only handle one image, so stop looking for more.
+                break
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+    
+    func sendURLRequest() {
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print("Action Extension")
+            print(response.request as Any)  // original URL request
+            print(response.response as Any) // HTTP URL response
+            print(response.data as Any)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
+
+}
